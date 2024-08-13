2b85db660084e61577f499854adb67810efbf39a
Arvin Ghaffari <arvinghaffari75@gmail.com>
2024-08-13 17:25:23 +0200

    Add react-router-dom and create routes with CreateBrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,41 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/Cart/Cart";
+import Order from "./features/Order/Order";
+import CreateOrder from "./features/Order/CreateOrder";
+import AppLayout from "./ui/AppLayout";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  // const x = 23;
-  return <div>Hello vite</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/ui/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <div>
+      <Link to="/">Back to root</Link>
+    </div>
+  );
+}
+
+export default Header;
