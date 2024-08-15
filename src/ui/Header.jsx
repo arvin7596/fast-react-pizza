0dabf0c32df9714852d61c2dc41d103c604da4e5
Arvin Ghaffari <arvinghaffari75@gmail.com>
2024-08-15 16:53:08 +0200

    add some style such as back ground color and spacing

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <div>
+    <div className="border-b border-stone-300 bg-yellow-500 p-4 font-semibold uppercase">
+      <h1 className="text text-lg tracking-widest">Fast react pizza co.</h1>
       <SearchOrder />
-      <Link to="/">Back to root</Link>
+      <Username />
     </div>
   );
 }
