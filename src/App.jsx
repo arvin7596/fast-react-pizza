15f8233ed02d5541733ca9193795fab309345f1d
Arvin Ghaffari <arvinghaffari75@gmail.com>
2025-01-07 23:17:32 +0100

    bug : fix text bug

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import Home from "./ui/Home";
 import Error from "./ui/Error";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
-import Order, { loader as orderLoader } from "./features/Order/Order";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import CreateOrder, {
   action as createOrderAction,
-} from "./features/Order/CreateOrder";
+} from "./features/order/CreateOrder";
 import { action as updateOrderAction } from "./features/order/UpdateOrder";
 import AppLayout from "./ui/AppLayout";
 
