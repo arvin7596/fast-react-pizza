8005fd4be2d281f8e6450624e6f0c8b801e4b983
Arvin Ghaffari <arvinghaffari75@gmail.com>
2025-01-07 23:21:04 +0100

    bug : fix text

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -9,7 +9,7 @@ import {
 } from "../../utils/helpers";
 import OrderItem from "./OrderItem";
 import { useEffect } from "react";
-import UpdateOrder from "./updateOrder";
+import UpdateOrder from "./UpdateOrder";
 
 function Order() {
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
