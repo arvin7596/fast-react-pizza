dc149c0af0979af90baacf13daa21860cac54a85
Arvin Ghaffari <arvinghaffari75@gmail.com>
2025-01-07 23:09:23 +0100

    bug : fix text bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./ui/Home";
 import Error from "./ui/Error";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
-import Cart from "./features/Cart/Cart";
+import Cart from "./features/cart/Cart";
 import Order, { loader as orderLoader } from "./features/Order/Order";
 import CreateOrder, {
   action as createOrderAction,
